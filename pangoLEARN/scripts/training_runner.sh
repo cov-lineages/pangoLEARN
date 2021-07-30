c884a8849fc40c9210f6137eaf6c5c68585e5cb8
aineniamh <aine.otoole@ed.ac.uk>
2021-07-30 10:53:39 +0100

    adding angie email to training pipeline

--- pangoLEARN/scripts/training_runner.sh
+++ pangoLEARN/scripts/training_runner.sh
@@ -33,6 +33,7 @@ echo "pango version $PANGO_V"
 
 cd /localdisk/home/shared/raccoon-dog/ #gets any updates to the reports in the data directory
 echo "--config outdir=$OUTDIR data_date=$DATA_DATE pangolearn_version=$TODAY pango_version=$PANGO_V"
+echo "pangoLEARN training starting" | mail -s "update lineageTree.pb" angie@soe.ucsc.edu
 snakemake --snakefile /localdisk/home/s1680070/repositories/pangoLEARN/pangoLEARN/scripts/curate_alignment.smk --rerun-incomplete --nolock --cores 1 --config outdir=$OUTDIR data_date=$DATA_DATE pangolearn_version=$TODAY pango_version=$PANGO_V
 
 cp /localdisk/home/shared/raccoon-dog/$OUTDIR/pangolearn.init.py   /localdisk/home/s1680070/repositories/pangoLEARN/pangoLEARN/__init__.py
