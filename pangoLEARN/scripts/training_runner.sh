f7da526074a65a620a794e4f40042b248620bf29
aineniamh <aine.otoole@ed.ac.uk>
2021-12-07 17:03:58 +0000

    generalising training runner

--- pangoLEARN/scripts/training_runner.sh
+++ pangoLEARN/scripts/training_runner.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+!/bin/bash
 
-#source /localdisk/home/s1680070/.bashrc
+#source /localdisk/home/shared/.bashrc
 #conda activate pangolin
 
 TODAY=$(date +%F)
@@ -19,24 +19,15 @@ fi
 
 echo "Training model version: $TODAY"
 
-if [ -z "$1" ]
-then
-    UID="s1680070"
-else
-    UID=$1
-fi
-
 if [ -z "$2" ]
 then
     DATA_DATE=$TODAY
 else
     DATA_DATE=$2
 fi
-echo "$UID training version $DATA_DATE"
+echo "Training version $DATA_DATE"
 
-# LATEST_DATA=$(ls -td /localdisk/home/shared/raccoon-dog/2021*_gisaid/publish/gisaid | head -n 1)
-
-REPO_PATH=/localdisk/home/$UID/repositories
+REPO_PATH=/localdisk/home/shared/repositories
 
 PANGO_PATH=$REPO_PATH/pango-designation
 PLEARN_PATH=$REPO_PATH/pangoLEARN
@@ -54,10 +45,20 @@ echo "pango version $PANGO_V"
 
 cd /localdisk/home/shared/raccoon-dog/ #gets any updates to the reports in the data directory
 echo "--config outdir=$OUTDIR data_date=$DATA_DATE pangolearn_version=$TODAY pango_version=$PANGO_V"
-echo "pangoLEARN training starting" | mail -s "update lineageTree.pb with pango designation version $PANGO_V" angie@soe.ucsc.edu
+# echo "pangoLEARN training starting" | mail -s "update lineageTree.pb with pango designation version $PANGO_V" angie@soe.ucsc.edu
 snakemake --snakefile $PLEARN_PATH/pangoLEARN/scripts/curate_alignment.smk --rerun-incomplete --nolock --cores 1 --config repo_path=$REPO_PATH outdir=$OUTDIR data_date=$DATA_DATE pangolearn_version=$TODAY pango_version=$PANGO_V
 
-# cp $OUTDIR/pangolearn.init.py   /localdisk/home/s1680070/repositories/pangoLEARN/pangoLEARN/__init__.py
-# cp $OUTDIR/decision*   /localdisk/home/s1680070/repositories/pangoLEARN/pangoLEARN/data/
-# cp $OUTDIR/metadata.downsample.csv   /localdisk/home/s1680070/repositories/pangoLEARN/pangoLEARN/data/lineages.downsample.csv
-# cp $OUTDIR/lineage.hash.csv   /localdisk/home/s1680070/repositories/pangoLEARN/pangoLEARN/data/lineages.hash.csv
+cp $OUTDIR/pangolearn.init.py   /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/__init__.py
+cp $OUTDIR/decision*   /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/data/
+cp $OUTDIR/metadata.downsample.csv   /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/data/lineages.downsample.csv
+cp $OUTDIR/lineage.hash.csv   /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/data/lineages.hash.csv
+
+cd /localdisk/home/shared/repositories/pangoLEARN && git pull
+git checkout auto_prerelease_$PANGO_V
+
+git add /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/__init__.py
+git add /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/data/decision*
+git add /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/data/lineages.downsample.csv
+git add /localdisk/home/shared/repositories/pangoLEARN/pangoLEARN/data/lineages.hash.csv
+
+git commit -m "auto prerelease of pangoLEARN for PANGO version $PANGO_V"
